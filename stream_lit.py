249867c9d718e643b20056d2f049c177069ad06e
a-newrag <146621808+a-newrag@users.noreply.github.com>
2023-12-26 21:06:44 +0530

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from urllib.error import URLError
+import pandas as pd
 @st.cache_data
 def get_UN_data():
     AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
