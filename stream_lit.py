3612aca76310262ea921bd49c753d99afd908f34
a-newrag <146621808+a-newrag@users.noreply.github.com>
2023-12-26 21:47:23 +0530

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -3,11 +3,11 @@ from urllib.error import URLError
 import pandas as pd
 import altair as alt
 
-@st.cache_data
+@st.cache_data #caches the return data; called before function definition
 def get_UN_data():
     AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
     df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-    return df.set_index("Region")
+    return df.set_index("Region") #sets Region column as the index
 
 try:
     df = get_UN_data()
